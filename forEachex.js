4ee50cc6f5bd236fefdca87988c7a1e1e19d2d27
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-02 16:44:16 +0000

    ajout des 2 fichier

---
+++ forEachex.js
@@ -0,0 +1,15 @@
+function findWaldo(arr, found) {
+  arr.forEach(function(element, i) {
+    if (element === "Waldo") {
+      found(i);
+    }
+  });
+}
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
